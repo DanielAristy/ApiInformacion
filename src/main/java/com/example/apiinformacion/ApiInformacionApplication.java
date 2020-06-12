dfa609fcc2afd75cb3dc29b8449db0a2007f9be6
DanielAristy <danielariaty@gmail.com>
2020-06-12 14:13:54 -0500

    Initial commit

---
+++ src/main/java/com/example/apiinformacion/ApiInformacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiinformacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInformacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInformacionApplication.class, args);
+	}
+
+}
