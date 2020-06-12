e30557066f85f370525ed70494084889dcc940de
DanielAristy <danielariaty@gmail.com>
2020-06-12 14:57:33 -0500

    EndPoints para el servicio que deseamos consumir.

--- src/main/java/com/example/apiinformacion/controller/VehiculoController.java
+++ src/main/java/com/example/apiinformacion/controller/VehiculoController.java
@@ -3,8 +3,7 @@ package com.example.apiinformacion.controller;
 import com.example.apiinformacion.entity.Vehiculo;
 import com.example.apiinformacion.service.VehiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,28 +18,40 @@ public class VehiculoController {
 
     /**Tansacciones*/
 
+    /**+ GetMapping Obtener recuersos
+     * + PostMapping para insertar
+     * + PutMapping
+     * + RequestBody en la peticion de la peticion Http vamos a enviar un objeto
+     * + PathVariable enviar variables por el protocolo Http
+     * */
+
     /**Buscar vehiculos por la placa*/
-    public Vehiculo findById(String placa){
+    @GetMapping("/{placa}/vehiculo")
+    public Vehiculo findById(@PathVariable String placa){
         return vehiculoService.findById(placa);
     }
 
     /**Insertar un vehiculo*/
-    public void insertar(Vehiculo vehiculo){
+    @PostMapping("/vehiculo")
+    public void insertar(@RequestBody Vehiculo vehiculo){
         vehiculoService.insert(vehiculo);
     }
 
     /**Actualizar vehiculo y su placa*/
-    public void actualizar(Vehiculo vehiculo, String placa){
+    @PutMapping("/{placa}/vehiculo")
+    public void actualizar(@RequestBody Vehiculo vehiculo, @PathVariable String placa){
         vehiculo.setPlaca(placa);
         vehiculoService.update(vehiculo);
     }
 
     /**Eliminar  un vehiculo por la placa*/
-    public void eliminar(String placa){
+    @DeleteMapping("/{placa}/vehiculo")
+    public void eliminar(@PathVariable String placa){
 
         vehiculoService.delete(placa);
     }
     /**listar vehiculos*/
+    @GetMapping("/vehiculos")
     public List<Vehiculo> listar(){
         return vehiculoService.findAll();
     }
